e1de081470af9ba5d6d11c6616771b6b3b8b71c0
Maxim Krasnov <mr.krasnovsky@gmail.com>
2023-06-15 14:07:28 +0300

    completed the exercise for lesson 11

---
+++ lesson11/src/main/resources/SOLUTION.sql
@@ -0,0 +1,4 @@
+UPDATE SUBJECT SET grade = 5 WHERE name = 'End of Suburbia: Oil Depletion and the Collapse of the American Dream';
+UPDATE STUDENT SET GROUPNUMBER = 8 WHERE NAME = 'Tremaine Worvill';
+UPDATE PAYMENT SET AMOUNT = 500, STUDENT_ID = 2 WHERE PAYMENT_DATE > '2021-01-01' and TYPE_ID = 2;
+UPDATE MARK SET MARK = 2 WHERE SUBJECT_ID  = 315;
